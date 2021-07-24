0ceb7a62b03a1c58d3c639c23d26b6eb65c5b809
sharmilasharmila <sra084@post.uit.no>
2021-07-24 14:14:12 +0530

    6.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +16,18 @@ export class AppComponent {
 // This is the function that will open the dialog when the signup button is clicked
   openUserRegistrationDialog(): void {
       this.dialog.open(UserRegistrationFormComponent, {
-
+        width: '280px'
       });
     }
     openUserLoginDialog(): void {
       this.dialog.open(UserLoginFormComponent, {
       width: '280px'
       });
+    }
+
+    openMoviesDialog(): void{
+      this.dialog.open(MovieCardComponent,{
+        width:'500px'
+      })
     }
 }
